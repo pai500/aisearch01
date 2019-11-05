d10e4d4b686223627940a20a3aa3727c0777c4e3
pai500 <pai500@gmail.com>
2019-11-05 12:49:41 +0700

    Updates

---
+++ precache-manifest.60668b9926791300d9f729fd6e92f3d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81659cc50dffce0d661ca943e58c7df8",
+    "url": "/aisearch/index.html"
+  },
+  {
+    "revision": "e576e79391314122cc36",
+    "url": "/aisearch/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6682763a2883aabfa539",
+    "url": "/aisearch/static/js/2.2b1620b6.chunk.js"
+  },
+  {
+    "revision": "e576e79391314122cc36",
+    "url": "/aisearch/static/js/main.9fc94ee2.chunk.js"
+  },
+  {
+    "revision": "5a871d035a28832b6f5a",
+    "url": "/aisearch/static/js/runtime-main.1850fda4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/aisearch/static/media/logo.25bf045c.svg"
+  }
+]);
